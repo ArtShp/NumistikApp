981147fb1c5ea0b6beb0002ca644139ebe061b8a
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-27 17:46:20 +0200

    feat: add Role entity
    Comes with a custom AuthorizeAttribute for authorization

---
+++ Server/Entities/Role.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Server.Entities;
+
+public enum Role
+{
+    User = 1,
+    Admin = 2,
+    Owner = 3
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+public class AuthorizeRoleAttribute : AuthorizeAttribute
+{
+    public AuthorizeRoleAttribute(params Role[] roles)
+    {
+        Roles = string.Join(",", roles.Select(r => Role.GetName(r)));
+    }
+}
