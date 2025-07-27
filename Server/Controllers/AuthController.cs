a2772ec1c94d5bd6415437952298942c3de55687
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-27 18:43:22 +0200

    feat!: add auth controller

---
+++ Server/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Entities;
+using Server.Models;
+using Server.Services;
+
+namespace Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(IAuthService authService) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserDto request)
+    {
+        var user = await authService.RegisterAsync(request);
+
+        if (user is null)
+            return Conflict("User with this username already exists. Try using another one.");
+
+        return StatusCode(201, user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<TokenResponseDto>> Login(UserDto request)
+    {
+        var result = await authService.LoginAsync(request);
+
+        if (result is null)
+            return Unauthorized("Invalid username or password.");
+
+        return Ok(result);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<ActionResult<TokenResponseDto>> RefreshToken(RefreshTokenRequestDto request)
+    {
+        var result = await authService.RefreshTokensAsync(request);
+
+        if (result is null || result.AccessToken is null || result.RefreshToken is null)
+            return Unauthorized("Invalid refresh token or user id.");
+
+        return Ok(result);
+    }
+}
