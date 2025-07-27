0811c3e829372e7841084bca43276b71c6049e58
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-27 17:47:07 +0200

    feat: add User entity

---
+++ Server/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public Role Role { get; set; } = Role.User;
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+}
