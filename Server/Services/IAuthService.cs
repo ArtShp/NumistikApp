cd88d69df3535d75fe3d80841134e9559b558f58
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-27 18:01:46 +0200

    feat: add auth service interface and implementation
    Provides authentication and authorization

---
+++ Server/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Server.Entities;
+using Server.Models;
+
+namespace Server.Services;
+
+public interface IAuthService
+{
+    Task<User?> RegisterAsync(UserDto request);
+    Task<TokenResponseDto?> LoginAsync(UserDto request);
+    Task<TokenResponseDto?> RefreshTokensAsync(RefreshTokenRequestDto request);
+}
