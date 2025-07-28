d9af2da6dbb075ee81ac5954546bb21fa0342cd2
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-28 11:52:50 +0200

    refactor(server): delete false annotations
    Delete User and InviteToken annotations from ASP.NET used there by mistake

--- Server/Entities/InviteToken.cs
+++ Server/Entities/InviteToken.cs
@@ -1,30 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Server.Entities;
 
 [Index(nameof(Token), IsUnique = true)]
 public class InviteToken
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
-    [Required]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Token { get; set; }
 
-    [Required]
     public required User CreatedBy { get; set; }
 
-    [Required]
     public DateTime CreatedAt { get; set; }
 
-    [Required]
     public DateTime ExpiresAt { get; set; }
 
-    [Required]
     public Role AssignedRole { get; set; }
 
     public User? UsedBy { get; set; }
--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -1,24 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Server.Entities;
 
 [Index(nameof(Username), IsUnique = true)]
 public class User
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
 
-    [Required]
-    [RegularExpression(@"^\w{3,30}$", ErrorMessage = "Username must be 3-30 characters and contain only letters, numbers, or underscores.")]
     public string Username { get; set; } = string.Empty;
 
-    [Required]
     public string PasswordHash { get; set; } = string.Empty;
 
-    [Required]
     public Role Role { get; set; } = Role.User;
 
     public string? RefreshToken { get; set; }
