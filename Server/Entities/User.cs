0c2dd1c90696ae77b8c18b8342bf3401e772d575
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-28 11:22:07 +0200

    refactor(server): update user db attributes
    Add some restrictions on the fields

--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -1,11 +1,25 @@
-﻿namespace Server.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
+namespace Server.Entities;
+
+[Index(nameof(Username), IsUnique = true)]
 public class User
 {
+    [Key]
     public Guid Id { get; set; }
+
+    [Required]
+    [RegularExpression(@"^\w{3,30}$", ErrorMessage = "Username must be 3-30 characters and contain only letters, numbers, or underscores.")]
     public string Username { get; set; } = string.Empty;
+
+    [Required]
     public string PasswordHash { get; set; } = string.Empty;
+
+    [Required]
     public Role Role { get; set; } = Role.User;
+
     public string? RefreshToken { get; set; }
+
     public DateTime? RefreshTokenExpiryTime { get; set; }
 }
