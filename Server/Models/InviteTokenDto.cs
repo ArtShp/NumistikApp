ff1ad002852404b00de607b1f76c7edc9d60cc7b
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-28 13:54:53 +0200

    feat(server): add InviteToken DTO

---
+++ Server/Models/InviteTokenDto.cs
@@ -0,0 +1,10 @@
+﻿using Server.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models;
+
+public class InviteTokenDto
+{
+    [Required]
+    public Role AssignedRole { get; set; }
+}
