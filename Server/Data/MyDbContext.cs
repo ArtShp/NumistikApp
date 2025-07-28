415ff628400e73e9a23247c207828116c18ab11c
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-28 11:54:29 +0200

    feat(server): add InviteToken to the Db context

--- Server/Data/MyDbContext.cs
+++ Server/Data/MyDbContext.cs
@@ -6,4 +6,5 @@ namespace Server.Data;
 public partial class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<InviteToken> InviteTokens { get; set; }
 }
