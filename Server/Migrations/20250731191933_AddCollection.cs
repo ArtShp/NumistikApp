4154adc466e02d0816e055ea1f75ccb346f6e881
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-07-31 21:22:08 +0200

    migration(server): add new migration
    - Add Collection

---
+++ Server/Migrations/20250731191933_AddCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Server.Entities;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:collection_role", "admin,editor,owner,viewer")
+                .Annotation("Npgsql:Enum:role", "admin,owner,user")
+                .OldAnnotation("Npgsql:Enum:role", "admin,owner,user");
+
+            migrationBuilder.CreateTable(
+                name: "Collections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCollections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CollectionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Role = table.Column<CollectionRole>(type: "collection_role", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCollections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserCollections_Collections_CollectionId",
+                        column: x => x.CollectionId,
+                        principalTable: "Collections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCollections_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCollections_CollectionId",
+                table: "UserCollections",
+                column: "CollectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCollections_UserId",
+                table: "UserCollections",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserCollections");
+
+            migrationBuilder.DropTable(
+                name: "Collections");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:role", "admin,owner,user")
+                .OldAnnotation("Npgsql:Enum:collection_role", "admin,editor,owner,viewer")
+                .OldAnnotation("Npgsql:Enum:role", "admin,owner,user");
+        }
+    }
+}
