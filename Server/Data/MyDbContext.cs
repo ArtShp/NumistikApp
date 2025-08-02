83d42c8e7dc471a792d8718b5ad3244a3ca6b06d
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-08-02 12:58:12 +0200

    feat(server): add CollectionItem entity

--- Server/Data/MyDbContext.cs
+++ Server/Data/MyDbContext.cs
@@ -13,6 +13,8 @@ public partial class MyDbContext(DbContextOptions<MyDbContext> options) : DbCont
     public DbSet<Currency> Currencies { get; set; }
     public DbSet<Country> Countries { get; set; }
     public DbSet<CatalogItem> CatalogItems { get; set; }
+    public DbSet<CollectionItem> CollectionItems { get; set; }
+    public DbSet<CollectionItemSpecialStatus> CollectionItemSpecialStatuses { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Server/Entities/CollectionItem.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Entities;
+
+public class CollectionItem
+{
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(CatalogItem))]
+    public int CatalogItemId { get; set; }
+
+    public CatalogItem CatalogItem { get; set; } = null!;
+
+    public CollectionItemStatus CollectionStatus { get; set; }
+
+    [ForeignKey(nameof(SpecialStatus))]
+    public int? SpecialStatusId { get; set; }
+
+    public CollectionItemSpecialStatus? SpecialStatus { get; set; }
+
+    public CollectionItemQuality? Quality { get; set; }
+
+    [ForeignKey(nameof(Collection))]
+    public Guid CollectionId { get; set; }
+
+    public Collection Collection { get; set; } = null!;
+
+    public string? SerialNumber { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? ObverseImageUrl { get; set; }
+
+    public string? ReverseImageUrl { get; set; }
+}
+
+[Index(nameof(Name), IsUnique = true)]
+public class CollectionItemSpecialStatus
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+}
+
+public enum CollectionItemStatus
+{
+    InCollection = 1,
+    ForSaleOrExchange = 2,
+    SoldOrExchanged = 3,
+    Lost = 4,
+}
+
+public enum CollectionItemQuality
+{
+    Uncirculated = 1,
+    ExtremelyFine = 2,
+    VeryFine = 3,
+    Fine = 4,
+    VeryGood = 5,
+    Good = 6,
+    Poor = 7
+}
