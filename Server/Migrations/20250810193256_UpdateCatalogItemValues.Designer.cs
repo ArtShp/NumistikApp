21b11b8353f2a3ee9b24bdc562589a2b0ebaafc7
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-08-10 21:59:39 +0200

    migration(server): update migrations

--- Server/Migrations/20250810193256_UpdateCatalogItemValues.Designer.cs
+++ Server/Migrations/20250810193256_UpdateCatalogItemValues.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Server.Data;
@@ -12,9 +13,11 @@ using Server.Entities;
 namespace Server.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250810193256_UpdateCatalogItemValues")]
+    partial class UpdateCatalogItemValues
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
