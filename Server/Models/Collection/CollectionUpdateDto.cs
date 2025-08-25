621c036312eaa023863847a95fd7c5b4f5ab574e
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-08-25 08:35:45 +0200

    feat(server): add DTO for Collection update

---
+++ Server/Models/Collection/CollectionUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.Models.Collection;
+
+public static class CollectionUpdateDto
+{
+    public class Request
+    {
+        public required Guid Id { get; set; }
+
+        public required string? Name { get; set; }
+
+        public required string? Description { get; set; }
+    }
+}
