fe1bbb51e9c9cb95b669e1c7d3cf9d12b23df7cf
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-08-29 14:31:10 +0200

    feat(app): add app settings class

---
+++ App/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace App;
+
+internal static class AppSettings
+{
+    private const string ServerUrlKey = "ServerUrl";
+
+    public static string ServerUrl
+    {
+        get => Preferences.Default.Get(ServerUrlKey, "");
+        set => Preferences.Default.Set(ServerUrlKey, value);
+    }
+}
