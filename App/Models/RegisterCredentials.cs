86fded1cae7ea0c6e961243bfc7acd14c2c58ee4
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-09-01 16:57:04 +0200

    feat(app): add register model

---
+++ App/Models/RegisterCredentials.cs
@@ -0,0 +1,15 @@
+﻿namespace App.Models;
+
+public class RegisterCredentials
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string InviteToken { get; set; }
+
+    public RegisterCredentials()
+    {
+        Username = string.Empty;
+        Password = string.Empty;
+        InviteToken = string.Empty;
+    }
+}
