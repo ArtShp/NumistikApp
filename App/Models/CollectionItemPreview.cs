bbeaa728310031d1fe62f088324f6d1cbb6e3cc8
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-09-07 15:50:39 +0200

    refactor(app): extend collection item preview

--- App/Models/CollectionItemPreview.cs
+++ App/Models/CollectionItemPreview.cs
@@ -4,9 +4,25 @@ public class CollectionItemPreview
 {
     public required int Id { get; set; }
     public required Guid CollectionId { get; set; }
-    public string? Value { get; set; }
-    public string? Currency { get; set; }
+    public required string Value { get; set; }
+    public required string Currency { get; set; }
+    public string? AdditionalInfo { get; set; }
     public string? SerialNumber { get; set; }
     public string? Description { get; set; }
     public string? ObverseImageUrl { get; set; }
+
+    public string ValueCurrencyInfo
+    {
+        get
+        {
+            var main = Value + ' ' + Currency;
+            if (!string.IsNullOrWhiteSpace(AdditionalInfo))
+            {
+                var info = AdditionalInfo;
+                return $"{main} ({AdditionalInfo})";
+            }
+
+            return main;
+        }
+    }
 }
