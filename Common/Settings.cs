59365e6e376a8297fb7412008d2ade6e608c9271
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-09-14 21:47:54 +0200

    feat(common): add expiration time settings

---
+++ Common/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace Common;
+
+public static class Settings
+{
+    public static TimeSpan AccessTokenExpiration => TimeSpan.FromMinutes(15);
+    public static TimeSpan RefreshTokenExpiration => TimeSpan.FromDays(30);
+}
