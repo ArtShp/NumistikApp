eb195d188c281a2f6b9d6ecf8c015b7f89672626
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2025-09-20 11:48:39 +0200

    refactor(app): add reverse image to item model

--- App/Models/CollectionItemPreview.cs
+++ App/Models/CollectionItemPreview.cs
@@ -10,6 +10,7 @@ public class CollectionItemPreview
     public string? SerialNumber { get; set; }
     public string? Description { get; set; }
     public string? ObverseImageUrl { get; set; }
+    public string? ReverseImageUrl { get; set; }
 
     public string? TypeName { get; set; }
     public string? CountryName { get; set; }
